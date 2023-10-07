9867537bc5ce21e641d3ed420b16c940bad72f13
Daniel Schmidt <info@daniel-schmidt.me>
2023-10-07 16:36:48 +0200

    Missing files.

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+	plugins: [tsconfigPaths()],
+	root: 'src/',
+	build: {
+		outDir: '../dist',
+	},
+	server: {
+		host: '',
+		port: 4000,
+	},
+});
